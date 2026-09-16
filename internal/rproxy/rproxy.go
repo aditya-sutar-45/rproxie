5418f80bbce1de13beb1f56ffa8b9e42e8903194
aditya-sutar-45 <aditya1.sutar@outlook.com>
2026-09-16 15:26:45 +0530

    refactored handler function

--- internal/rproxy/rproxy.go
+++ internal/rproxy/rproxy.go
@@ -2,10 +2,12 @@
 package rproxy
 
 import (
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/aditya-sutar-45/rproxie/internal/backend"
+	"github.com/aditya-sutar-45/rproxie/internal/utils"
 )
 
 type ReverseProxy struct {
@@ -42,4 +44,51 @@ func (rp *ReverseProxy) Start() error {
 	}
 
 	return nil
+}
+
+func (rp *ReverseProxy) handler(w http.ResponseWriter, r *http.Request) {
+	request, err := rp.newRequest(r, rp.backends[0])
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadGateway, "backend unavailable")
+		return
+	}
+
+	resp, err := rp.client.Do(request)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadGateway, "backend unavailable")
+		return
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	rp.writeResponse(w, resp)
+}
+
+func (rp *ReverseProxy) newRequest(r *http.Request, backend *backend.Backend) (*http.Request, error) {
+	targetURL := *backend.URL
+	targetURL.Path = r.URL.Path
+	targetURL.RawQuery = r.URL.RawQuery
+
+	request, err := http.NewRequest(r.Method, targetURL.String(), r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header = r.Header.Clone()
+
+	return request, nil
+}
+
+func (rp *ReverseProxy) writeResponse(w http.ResponseWriter, resp *http.Response) {
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+	_, err := io.Copy(w, resp.Body)
+	if err != nil {
+		log.Printf("ERROR streaming response to client: %v", err)
+	}
 }
